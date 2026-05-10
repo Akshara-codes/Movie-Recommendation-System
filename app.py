d3f0eb9e603f3c6a69341d6873b67507a7c6fe08
Akshara-codes <aksharajain030@gmail.com>
2026-05-10 15:47:13 +0530

    Resolved error in app.py

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import zipfile
-import os
 
-if not os.path.exists("similarity.pkl"):
-    with zipfile.ZipFile("similarity.zip", "r") as zip_ref:
-        zip_ref.extractall(".")
 def recommend(movie):
-    movie_index =movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-     # to hold the index
-    recommended_movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
-    recommended_movies=[]
+    movie_index = movies[movies['title'] == movie].index[0]
+    indices, scores = similarity[movie_index]
     
-    for i in recommended_movies_list:
-        #movie_id=i[0]
-        # fetch poster from api
-        recommended_movies.append(movies.iloc[i[0]].title)
-        
+    recommended_movies = []
+    for idx in indices:
+        if idx != movie_index: 
+            recommended_movies.append(movies.iloc[idx].title)
+        if len(recommended_movies) == 5:
+            break
     return recommended_movies
     
-similarity=pickle.load(open('similarity.pkl','rb'))       
+    
+similarity = pickle.load(open('similarity_compressed.pkl', 'rb'))
 movies = pickle.load(open('movies.pkl','rb'))
 
 movie_titles = movies['title'].values
@@ -32,6 +26,6 @@ movie=st.selectbox('Which movie would you like to watch? ',movie_titles)
 
 if st.button('Recommend'):
     recommendations=recommend(movie)
-    
-    for i in recommendations:
-        st.write(i)
+    st.subheader("Top 5 Recommendations:")
+    for i, rec in enumerate(recommendations, 1):
+        st.write(f"{i}. {rec}")
