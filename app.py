3e3feb13c2f440d2c93de431fde7d247a42fbaf8
Akshara-codes <aksharajain030@gmail.com>
2026-05-10 17:09:57 +0530

    Resolved errors in  app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import streamlit as st
 import pickle
-import pandas as pd
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     indices, scores = similarity[movie_index]
     
     recommended_movies = []
+    recommended_posters = []
+    
     for idx in indices:
-        if idx != movie_index: 
+        if idx != movie_index:
             recommended_movies.append(movies.iloc[idx].title)
+            recommended_posters.append(movies.iloc[idx].poster_url)
         if len(recommended_movies) == 5:
             break
-    return recommended_movies
     
-    
-similarity = pickle.load(open('similarity_compressed.pkl', 'rb'))
-movies = pickle.load(open('movies.pkl','rb'))
+    return recommended_movies, recommended_posters
 
+similarity = pickle.load(open('similarity_compressed.pkl', 'rb'))
+movies = pickle.load(open('movies_with_posters.pkl', 'rb'))
 movie_titles = movies['title'].values
 
 st.title('🎬 Movie Recommender System')
