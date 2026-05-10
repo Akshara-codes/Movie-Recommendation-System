4807c7767f033ac1d642ab234d63c936d8fb4906
Akshara-codes <aksharajain030@gmail.com>
2026-05-10 12:54:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
 import streamlit as st
-import pickle
-import pandas as pd
 
 
 
-movies = pickle.load(open('movies.pkl','rb'))
-
-movie_titles = movies['title'].values
 
 st.title('Movie Recommender System')
 
-movie=st.selectbox('Which movie would you like to watch? ',movie_titles)
-
-if st.button('Recommend'):
-    recommendations=recommend(movie)
-    
-    for i in recommendations:
-        st.write(i)
